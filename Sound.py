a9279104e156f89b019e344c241e56c47a76645b
Owen Exon <o.exon@outlook.com>
2025-08-10 12:02:59 +0100

    Corrected code and added GIF output

--- Sound.py
+++ Sound.py
@@ -1,10 +1,11 @@
-from PIL import Image
+# from PIL import Image
 import numpy as np
 import math
+import imageio.v2 as imageio
 
 soundPoints = [
-    (-3,0),
-    # (3,0)
+    (3,3),
+    (-3,0)
 ]
 
 def distance(p1:tuple,p2:tuple):
@@ -15,14 +16,16 @@ def distance(p1:tuple,p2:tuple):
 resolution = 256
 width = 10
 center = (0,0)
-wavelength = 1
-frames = 120
-timeIncrement = 0.1
+wavelength = 2
+frames = 30
+timeIncrement = (2*math.pi)/(frames+1)
 
-strengthPerPoint = 1/len(soundPoints)
+strengthPerPoint = 0.5/len(soundPoints)
 increment = width / resolution
 initX , initY = center[0]-width/2 , center[1]+width/2
 x , y = initX , initY
+
+images = []
 
 for frame in range(frames):
     x , y = initX , initY
@@ -33,8 +36,8 @@ for frame in range(frames):
             strength = 0
             for emitter in soundPoints:
                 pointDistance = distance((x,y),(emitter))
-                strength += math.sin( 2 * math.pi * (pointDistance % wavelength) / wavelength - (frame*timeIncrement) ) * strengthPerPoint
-            pixelStrength = abs(strength) * 255
+                strength += math.sin( (2 * math.pi * (pointDistance % wavelength) / wavelength) - (frame*timeIncrement) ) * strengthPerPoint
+            pixelStrength = (strength + 0.5) * 255
             tempPix.append((pixelStrength,pixelStrength,pixelStrength))
             x += increment
         
@@ -43,7 +46,8 @@ for frame in range(frames):
         x = initX
         y -= increment
         
-    array = np.array(pix,dtype=np.uint8)
-
-    newImage = Image.fromarray(array)
-    newImage.save(f"Image{frame}.png")
+    images.append(pix)
+    print(frame)
+    
+frames = [np.array(img,dtype=np.uint8) for img in images]
+imageio.mimsave("Output.gif", frames, fps=15,loop=0)
